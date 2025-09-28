c49943ab5f77462d531ea932fb382b4a3e98318b
Gabriel Duran <gabrieldurans999@gmail.com>
2025-09-28 10:04:05 -0300

    Se agrega detailActivity a FavMovie para al presionar las cartas

--- app/src/main/java/com/example/appconapi/Data/FavoriteList.kt
+++
@@ -1,9 +0,0 @@
-package com.example.appconapi.Data
-
-data class FavoriteList(
-
-    val listFavMovies : MutableList<MovieItem>, //Lista de las pelicula
-    val cantMovies : Int //Cantidad de peliculas
-
-
-)
--- app/src/main/java/com/example/appconapi/Vistas/FavMovieActivity.kt
+++ app/src/main/java/com/example/appconapi/Vistas/FavMovieActivity.kt
@@ -1,6 +1,9 @@
 package com.example.appconapi.Vistas
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,12 +12,17 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.appconapi.Adaptador.MovieAdapter
 import com.example.appconapi.Data.FavoriteManager
+import com.example.appconapi.Funciones.fetchMovie
 import com.example.appconapi.R
 
 class FavMovieActivity : AppCompatActivity() {
     //Inicializacion
     private lateinit var recyclerView: RecyclerView //guardadndo recycle
     private lateinit var adapter: MovieAdapter //guardando adaptador
+
+    private lateinit var editTextMovie: EditText //busqueda de pelicula lateiinit var
+
+    private lateinit var buttonSeeach : Button //boton de buscar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,17 +31,27 @@ class FavMovieActivity : AppCompatActivity() {
 
         recyclerView = findViewById(R.id.recyclerViewFav)
         recyclerView.layoutManager = LinearLayoutManager(this)
+        buttonSeeach = findViewById(R.id.buttonSearchFavMovie)
 
         // Cargar las películas desde FavoriteManager
         val favoriteMovies = FavoriteManager.favorites
 
-        // Pasamos la lista y definimos qué hacer al hacer click
-        adapter = MovieAdapter(favoriteMovies) { movie ->
-            // Aquí decides qué pasa cuando tocas un favorito
-            Toast.makeText(this, "Seleccionaste: ${movie.Title}", Toast.LENGTH_SHORT).show()
-        }
 
+        adapter = MovieAdapter(favoriteMovies) { movie -> //Carta que muestra pelicula
+            // 👉 Redirigir a DetailActivity con los datos de la película
+            val intent = Intent(this, DetailActivity::class.java).apply {
+                putExtra("MOVIE_TITLE", movie.Title)
+                putExtra("MOVIE_YEAR", movie.Year)
+                putExtra("USE_YEAR_FILTER", true) // para que use fetchMovieAndYear
+            }
+            startActivity(intent)
+        }
         recyclerView.adapter = adapter
+
+        //Busqueda de pelicula favorito
+        buttonSeeach.setOnClickListener {
+
+        }
     }
 
 }
